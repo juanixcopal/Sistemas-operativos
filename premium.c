fbdf053853006042d0d8478b618b4f99259ec003
juan.ixcopal <juan.ixcopal@ct.uneatlantico.es>
2025-06-02 13:13:32 +0200

    With using fork

--- premium.c
+++ premium.c
@@ -9,31 +9,33 @@
 #include <pthread.h>
 pthread_mutex_t lock;
 pthread_t ping_t, pong_t;
-void* ping(void* args);
-void* pong(void* args);
+void *ping(void *args);
+void *pong(void *args);
 int *contador = 0;
-int *flag= 0;
+int *flag = 0;
 int pipe1[2];
-void cantidad(int n) {
+void cantidad(int n)
+{
 	int cantidad = *contador;
 	printf("cantidad ping y pongs %d\n", cantidad);
 	write(pipe1[1], &cantidad, sizeof(int));
 	pthread_cancel(pong_t);
 	pthread_cancel(ping_t);
-	*flag=1;
+	*flag = 1;
 }
 int main()
-{	int n = 1;
+{
+	int n = 1;
 	pipe(pipe1);
 	signal(SIGINT, cantidad);
-	contador  = mmap(NULL, sizeof* contador,
-	                 PROT_READ | PROT_WRITE,
-	                 MAP_SHARED | MAP_ANONYMOUS,
-	                 -1, 0);
-	flag  = mmap(NULL, sizeof* flag,
-	             PROT_READ | PROT_WRITE,
-	             MAP_SHARED | MAP_ANONYMOUS,
-	             -1, 0);
+	contador = mmap(NULL, sizeof *contador,
+					PROT_READ | PROT_WRITE,
+					MAP_SHARED | MAP_ANONYMOUS,
+					-1, 0);
+	flag = mmap(NULL, sizeof *flag,
+				PROT_READ | PROT_WRITE,
+				MAP_SHARED | MAP_ANONYMOUS,
+				-1, 0);
 	*contador = 1;
 	*flag = 0;
 	sigset_t sig;
@@ -45,50 +47,64 @@ int main()
 	pthread_create(&pong_t, NULL, &pong, NULL);
 	usleep(1);
 	pthread_kill(ping_t, SIGUSR1);
-	read(pipe1[0], &n,sizeof(int));
-	FILE *archivo;
-	archivo = fopen("test.txt", "w");
-	fprintf(archivo, "total de pings y pongs: %d", n);
-	fclose(archivo);
 	pthread_join(ping_t, NULL);
 	pthread_join(pong_t, NULL);
+	pid_t hijo = fork();
+	if (hijo == 0)
+	{
+		read(pipe1[0], &n, sizeof(int));
+		FILE *archivo;
+		archivo = fopen("test.txt", "w");
+		fprintf(archivo, "total de pings y pongs: %d", n);
+		fclose(archivo);
+	}
+	wait(&hijo);
 	return 0;
 }
-void* ping(void* args) {
+void *ping(void *args)
+{
 	int n = 0;
 	sigset_t sig;
 	sigemptyset(&sig);
 	sigaddset(&sig, SIGUSR1);
-	while(1) {
-		if(*flag==0) {
-			sigwait(&sig,  &n);
+	while (1)
+	{
+		if (*flag == 0)
+		{
+			sigwait(&sig, &n);
 			pthread_mutex_lock(&lock);
 			printf("ping \n");
 			sleep(1);
-			*contador= *contador + 1;
+			*contador = *contador + 1;
 			pthread_kill(pong_t, SIGUSR2);
 			pthread_mutex_unlock(&lock);
 		}
-		else {
+		else
+		{
 			return NULL;
 		}
 	}
 }
-void* pong(void* args) {
+void *pong(void *args)
+{
 	int n = 0;
 	sigset_t sig;
 	sigemptyset(&sig);
 	sigaddset(&sig, SIGUSR2);
-	while(1) {
-		if(*flag==0) {
-			sigwait(&sig,  &n);
+	while (1)
+	{
+		if (*flag == 0)
+		{
+			sigwait(&sig, &n);
 			pthread_mutex_lock(&lock);
 			printf("pong \n");
 			sleep(1);
-			*contador= *contador + 1;
+			*contador = *contador + 1;
 			pthread_kill(ping_t, SIGUSR1);
 			pthread_mutex_unlock(&lock);
-		} else {
+		}
+		else
+		{
 			return NULL;
 		}
 	}
